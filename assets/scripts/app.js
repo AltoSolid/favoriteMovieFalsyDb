673279277b10fca7e180da1de0f9f8bc05132c3b
ZeroAptitude <altosolid@gmail.com>
2021-05-15 22:04:07 -0500

    User Input can now be entered and is stored in an array

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,12 @@ const addMovieModal = document.getElementById("add-modal")
 const startAddMovieBtn = document.querySelector("header button");
 
 const backdrop = document.getElementById("backdrop");
+
 const cancelAddMovieBtn = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieBtn = cancelAddMovieBtn.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
+
+const movies = [];
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle("visible");
@@ -17,10 +22,44 @@ const backdropClickHandler = () => {
     toggleMovieModal();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
     toggleMovieModal();
+    clearMovieInput();
+};
+
+const clearMovieInput = () => {
+    for (const userInput of userInputs) {
+        userInput.value = "";
+    }
+};
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if (titleValue.trim() === "" ||
+        imageUrlValue.trim() === "" ||
+        ratingValue.trim() === "" ||
+        +ratingValue < 1 ||
+        +ratingValue > 5
+    ) {
+        alert("Please enter valid values (rating between 1 and 5).");
+    }
+
+    const newMovie = {
+        title: titleValue,
+        image: imageUrlValue,
+        rating: ratingValue,
+    };
+
+    movies.push(newMovie);
+    console.log(movies);
+    toggleMovieModal();
+    clearMovieInput();
 };
 
 startAddMovieBtn.addEventListener("click", toggleMovieModal);
 backdrop.addEventListener("click", backdropClickHandler);
-cancelAddMovieBtn.addEventListener("click", cancelAddMovie);
+cancelAddMovieBtn.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieBtn.addEventListener("click", addMovieHandler)
